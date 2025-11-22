7afa321e2c6c614894a7f53b23c388add01f0ee3
Marvin <marvinbernard2018@gmail.com>
2025-11-22 15:24:34 +0300

    Added bootstap the correct way

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
+// import variables. and global.css
 import './styles/variables.css'
 import './styles/globals.css'
 import { AuthProvider } from './context/AuthContext.jsx'
+import "bootstrap/dist/css/bootstrap.min.css"; // <--Bootsrap CSS
+import "bootstrap/dist/js/bootstrap.bundles.min.js"; // <--optional JS
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
