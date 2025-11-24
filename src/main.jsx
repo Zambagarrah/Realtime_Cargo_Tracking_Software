1b79969c5246dd329af2fbdf3964c0ab0b4146ee
Zambagarrah <zambagarrah@users.noreply.github.com>
2025-11-24 12:39:41 +0300

    Added React Toastify import  to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import './styles/variables.css'
-import './styles/globals.css'
 import { AuthProvider } from './context/AuthContext.jsx'
 import AppRouter from './router/routes.jsx' 
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import './styles/variables.css'
+import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <AppRouter /> 
+      <ToastContainer position="top-right" autoClose={3000} />
     </AuthProvider>
   </React.StrictMode>
 )
